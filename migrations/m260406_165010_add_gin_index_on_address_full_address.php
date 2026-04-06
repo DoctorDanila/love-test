b9e3ee56a30f7f4798413537cf13651af1a23d90
Кузнецов Данила Алексеевич <50800526+DoctorDanila@users.noreply.github.com>
2026-04-06 21:53:52 +0500

    #7 feat: add gin index migr

---
+++ migrations/m260406_165010_add_gin_index_on_address_full_address.php
@@ -0,0 +1,29 @@
+<?php
+
+use yii\db\Migration;
+
+class m260406_165010_add_gin_index_on_address_full_address extends Migration
+{
+    public $transactional = false;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->execute('CREATE EXTENSION IF NOT EXISTS pg_trgm');
+
+        $this->execute(
+            'CREATE INDEX CONCURRENTLY IF NOT EXISTS idx_address_full_address_gin 
+             ON address USING gin (full_address gin_trgm_ops)'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->execute('DROP INDEX IF EXISTS idx_address_full_address_gin');
+    }
+}
